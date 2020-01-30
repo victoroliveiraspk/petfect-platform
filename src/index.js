df1cb3adc4a54bd0522a3ef2396608a873a115c1
victor <victoroliveiraspk@gmail.com>
2020-01-30 01:03:38 -0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Entrypoint from './components/Entrypoint.jsx';
+
+ReactDOM.render(
+    <Entrypoint />,
+    document.getElementById('app')
+);
